f06d0431921ab44b5413e499f8389f6ee6c61287
Rea <andrea.solisg@gmail.com>
2023-02-02 15:12:21 +0100

    premier commit

---
+++ app.rb
@@ -0,0 +1,71 @@
+require 'bundler'
+#Bundler.require
+#require 'pry'
+
+
+require_relative 'lib/Game.rb'
+require_relative 'lib/Player'
+require_relative 'lib/Board'
+
+
+puts "Joueur 1 : Quel est ton prenom ?"
+name1 = gets.chomp
+
+puts 'Choisis "X" ou "O"'
+sign1 = gets.chomp
+
+while sign1 != "O" && sign1 != "X"
+    puts 'Malin toi !, cest "X" ou "O"'
+    sign1 = gets.chomp
+end
+
+puts "Joueur 2 : Quel est ton prenom ?"
+name2 = gets.chomp
+
+puts 'Choisis "X" ou "O"'
+sign2 = gets.chomp
+
+while sign2 != "O" && sign2 != "X"
+    puts 'Malin toi !, cest "X" ou "O"'
+    sign1 = gets.chomp
+end
+
+player1 = Player.new(name1,name2)
+player2 = Player.new(sign1,sign2)
+
+puts "Board to be alive !"
+
+board = Board.new
+board.show_board
+
+while  board.cases.include? " "
+    board.player_choise(player1)
+    board.show_board
+    board.player_choise(player2)
+    board.show_board
+end
+
+
+
+#binding.pry
+
+
+
+
+    
+   
+  
+  
+
+
+ 
+    
+    
+
+
+
+
+
+
+
+
---
+++ lib/Board.rb
@@ -0,0 +1,61 @@
+#require 'pry'
+class Board
+    attr_accessor :a1, :a2, :a3, :b1, :b2, :b3,:c1, :c2, :c3, :cases
+    
+    def initialize
+        @a1 = " "
+        @a2 = " "
+        @a3 = " "
+        @b1 = " "
+        @b2 = " "
+        @b3 = " "
+        @c1 = " "
+        @c2 = " "
+        @c3 = " "
+        
+        @cases = [@a1, @a2, @a3, @b1, @b2, @b3, @c1, @c2, @c3]
+      end
+
+    def show_board
+        puts "       1           2          3"
+        puts "A      #{@cases[0]}     |    #{@cases[1]}     |     #{@cases[2]}"
+        puts "   ______________________________"
+        puts "B      #{@cases[3]}     |    #{@cases[4]}     |     #{@cases[5]}"
+        puts "   ______________________________"
+        puts "C      #{@cases[6]}     |    #{@cases[7]}     |     #{@cases[8]}"
+    end
+
+    def player_choise(player)
+        puts 'Lache une caise'
+        choise_player = gets.chomp
+
+        case choise_player
+            when "a1"
+                @cases[0] = player.sign
+            when "a2"
+                @cases[1] = player.sign
+            when "a3"
+                @cases[2]= player.sign
+            when "b1"
+                @cases[3] = player.sign
+            when "b2"
+                @cases[4] = player.sign
+            when "b3"
+                @cases[5] = player.sign
+            when "c1"
+                @cases[6] = player.sign
+            when "c2"
+                @cases[7] = player.sign
+            when "c3"
+                @cases[8]= player.sign
+            end
+        end
+
+
+end
+
+#board.player_choise(choise,player1)
+
+#binding.pry
+
+# Board.new.show_board
