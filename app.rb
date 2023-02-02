3e5d384363f9ebcec8ee068ff28a21022c8e56da
Rea <andrea.solisg@gmail.com>
2023-02-02 15:52:10 +0100

    choix des signes resolu

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ name1 = gets.chomp
 
 puts 'Choisis "X" ou "O"'
 sign1 = gets.chomp
-
+                                      
 while sign1 != "O" && sign1 != "X"
     puts 'Malin toi !, cest "X" ou "O"'
     sign1 = gets.chomp
@@ -22,13 +22,10 @@ end
 puts "Joueur 2 : Quel est ton prenom ?"
 name2 = gets.chomp
 
-puts 'Choisis "X" ou "O"'
-sign2 = gets.chomp
+array =["X","O"].delete(sign1)
+sign2 = array[0]
 
-while sign2 != "O" && sign2 != "X"
-    puts 'Malin toi !, cest "X" ou "O"'
-    sign1 = gets.chomp
-end
+puts "#{name2} ton sign est: #{sign2} "
 
 player1 = Player.new(name1,name2)
 player2 = Player.new(sign1,sign2)
